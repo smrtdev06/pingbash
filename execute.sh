67729498b23641f24ba2381162a472925f536a3e
smrtdev06 <smrtdev06@gmail.com>
2025-09-17 15:03:54 -0700

    banned user logic fixed

--- execute.sh
+++ execute.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Ensure environment variables are loaded
+source ~/.bashrc
+git pull
 pm2 restart 8
 cd MayaIQ_F-main
 npm run build
@@ -5,3 +10,6 @@ cd ..
 cd MayaIQ_W-main
 npm run build
 cd ..
+
+pm2 restart 9
+pm2 restart 10
