50750423fc95eff0e3cdd78ecf5f90b07ebf7146
Sweet <102741052+smartcoder0305@users.noreply.github.com>
2025-12-18 18:01:58 +0900

    Revert changes.

--- MayaIQ_F-main/src/components/landingPage/welcome.tsx
+++ MayaIQ_F-main/src/components/landingPage/welcome.tsx
@@ -10,7 +10,7 @@ const Welcome: React.FC = () => {
       <div className="text-container flex flex-col items-center w-[1120px] mx-auto max-[1135px]:w-[620px] max-[650px]:w-[85%] gap-[60px] px-[80px] max-[1135px]:px-0 pt-[50px] ">
         <div className="flex flex-col items-center gap-[10px] max-[650px]:w-full">
           <div className="text-[48px]  max-[650px]:text-[32px] max-[1135px]:leading-[51.36px] text-white text-center font-semibold leading-[85.6px]">
-            A Free Chatbox for your website...
+            A Free Chatbox for your website
           </div>
           <div className=" text-[22px] leading-[28.8px] font-semibold text-[#9CA3AF] max-[1135px]:text-[16px] max-[1135px]:leading-[25.6px]">
             Create a free group chatbox in seconds
